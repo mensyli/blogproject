00d1ff5e5fab587a663ff8d39e54e6b7487fad97
mensyli <mensyli@sina.com>
2017-06-10 15:57:20 +0800

    'nineth'

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.postDetailView.as_view(), name='detail'),
     # url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.archives,name='archives'),
-    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.ArchivesView as_view(),name='archives'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.ArchivesView.as_view(),name='archives'),
     # url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category')
-    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView as_view(), name='category')
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category')
 ]
